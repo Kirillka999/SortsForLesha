cfde143e80b987f17fad94a91b89ffb284805b31
Kirill <kirill93549@gmail.com>
2025-06-18 14:30:20 +0300

    Optimized bubble sort using StalinInsertion sort

---
+++ LeshaMergeQuickSort/MySorts2.cs
@@ -0,0 +1,89 @@
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace LeshaMergeQuickSort;
+
+public class MySorts2
+{
+
+    static public List<int> LeshaStalinInsertionSort(List<int> listToSort)
+    {
+        List<int> killedList = new List<int>();
+        List<int> emptyIndexes = new List<int>();
+        
+        int lastAccepted = listToSort[0];
+        
+        for (var i = 1; i < listToSort.Count; i++)
+        {
+            if (listToSort[i] < lastAccepted)
+            {
+                killedList.Add(listToSort[i]);
+                emptyIndexes.Add(i);
+            }
+            else
+            {
+                lastAccepted = listToSort[i];
+            }
+        }
+
+        var sortedList = LeshaInsertionSort(killedList);
+        
+        for (var i = 0; i < emptyIndexes.Count; i++)
+        {
+            listToSort[emptyIndexes[i]] = sortedList[i];
+        }
+        
+
+        return listToSort;
+    }
+    
+    
+    static public List<int> LeshaInsertionSort(List<int> listToSort)
+    {
+        for (var i = 1; i < listToSort.Count; i++)
+        {
+            int key = listToSort[i];
+            int index = i;
+            while (index > 0 && listToSort[index - 1] > key)
+            {
+                listToSort[index] = listToSort[index - 1];
+                //listToSort[index - 1] = key;
+                index--;
+            }
+
+            listToSort[index] = key;
+        }
+
+        return listToSort;
+    }
+
+    static public List<int> LeshaBubbleSort(List<int> listToSort)
+    {
+        bool exchangeHappened;
+        int iterator = 0;
+        for (var i = 0; i < listToSort.Count; i++)
+        {
+            exchangeHappened = false;
+            for (var i1 = 0; i1 < listToSort.Count - 1 - i; i1++)
+            {
+                if (listToSort[i1 + 1] < listToSort[i1])
+                {
+                    int c;
+                    c = listToSort[i1];
+                    listToSort[i1] = listToSort[i1 + 1];
+                    listToSort[i1 + 1] = c;
+                    exchangeHappened = true;
+                    iterator++;
+                }
+            }
+            if (!exchangeHappened)
+            {
+                Console.WriteLine(iterator + " exchanges happened in bubble sort");
+                break;
+            }
+            
+        }
+
+        return listToSort;
+    }
+}
--- LeshaMergeQuickSort/Program.cs
+++ LeshaMergeQuickSort/Program.cs
@@ -4,18 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> testArray = new List<int>(300);
-        for (var i = 0; i < 300; i++)
-        {
-            testArray.Add(Random.Shared.Next(1, 21));
-        }
+        // List<int> testArray = new List<int>(300);
+        // for (var i = 0; i < 300; i++)
+        // {
+        //     testArray.Add(Random.Shared.Next(1, 21));
+        // }
+        //List<int> testArray = new List<int>() { 1, 2, 11, 4, 5, 9, 7, 8, 6, 10, 3, 12, 13 };
+        //List<int> testArray = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
         
+        List<int> testArray = new List<int>() { 7, 2, 11, 4, 9, 1, 13, 6, 3, 12, 5, 10, 8 };
+
         Print(testArray);
         Console.WriteLine();
         Console.WriteLine();
-        var sortedArr = MySorts.LeshaMergeQuickSort(testArray);
+        var sortedArr = MySorts2.LeshaStalinInsertionSort(testArray);
         Print(sortedArr);
+        Console.WriteLine();
+        var sortedArr2 = MySorts2.LeshaBubbleSort(testArray);
+        Print(sortedArr2);
 
+
+        
     }
 
     static void Print(List<int> arrayToPrint)
